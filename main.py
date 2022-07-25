cdd946bb6ceca284ffe8f9a6c7ffb16f960afc9d
Sylvester Abiah <malonekeller@gmail.com>
2022-07-25 21:23:15 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+app = FastAPI(title="Article Search Engine API")
+
+
+@app.on_event("startup")
+def on_startup():
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+    nltk.download('omw-1.4')
+    
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+data = pd.read_csv('articles.csv')
+data['Title'] = data['Title'].apply(lambda x: x.lower())
+
+
+stopwords = nltk.corpus.stopwords.words('english')
+extra = ['!', '(', ')', '-', '[', ']', '{', '}', ';', ':', '"', ',',
+         '<', '>', '/', '?', '@', '#', '$', '%', '^', '&', '*', '_', '~', "'"]
+
+lemmatizer = WordNetLemmatizer()
+
+
+def stopword_tokenize(sentence):
+    # tokenize
+    word_list = word_tokenize(sentence)
+    # remove stop words
+    stop_list = [
+        word for word in word_list if word not in stopwords and word not in extra and word.isalpha()]
+    # lemmatize result
+    final_list = [lemmatizer.lemmatize(word) for word in stop_list]
+    return final_list
+
+
+vectorizer = TfidfVectorizer(tokenizer=stopword_tokenize)
+
+
+def cosine_sim(text1, query):
+    tfidf = vectorizer.fit_transform([text1, query])
+    return round(((tfidf * tfidf.T).A)[0, 1], 2)
+
+
+@app.get("/api")
+def get_result(query: str):
+    data['Cosine_sim'] = data['Title'].apply(lambda x: cosine_sim(x, query))
+    result = data.sort_values(by='Cosine_sim', ascending=False).head(10).values.tolist()
+    return result
